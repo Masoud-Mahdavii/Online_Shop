b519a91c036edcaeaa8d9d3f97d57f031512f914
masoud mahdavi <masoudmahdavi1993@gmail.com>
2025-10-28 10:01:21 +0330

    Updating product quantities in the cart

--- myshop/cart/urls.py
+++ myshop/cart/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from . import views
+from .views import cart_detail, cart_add, cart_remove
 
-app_name = 'cart'  # for namespacing
+app_name = 'cart'
 
 urlpatterns = [
-    path('', views.cart_detail, name='cart_detail'),
-    path('add/<int:product_id', views.cart_add, name='cart_add'),
-    path('remove/<int:product_id>', views.cart_remove, name='cart_remove'),
+    path('', cart_detail, name='cart_detail'),
+    path('add/<int:product_id>', cart_add, name='cart_add'),
+    path('remove/<int:product_id>', cart_remove, name='cart_remove'),
     # path('clear/', views.cart_clear, name='cart_clear'),
     # path('summary/', views.cart_summary, name='cart_summary'),
 ]
