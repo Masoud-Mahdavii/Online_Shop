bd74bd57630562a2592c35033c4906934771ae00
masoud mahdavi <masoudmahdavi1993@gmail.com>
2025-11-05 10:43:42 +0330

    Creating customer orders

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from cart.cart import Cart
+from .forms import OrderCreateForm
+from .models import OrderItem, Order
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            # clear the cart
+            cart.clear()
+            return render(request, 'orders/order/created.html', {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+@login_required
+def order_list(request):
+    orders = Order.objects.filter(user=request.user)
+    return render(request, 'orders/order/list.html', {'orders': orders})
+
+@login_required
+def order_detail(request, order_id):
+    order = Order.objects.get(id=order_id)
+    return render(request, 'orders/order/detail.html', {'order': order})
+
+@login_required
+def order_confirm(request, order_id):
+    order = Order.objects.get(id=order_id)
+    order.status = 'confirmed'
+    order.save()
+    return redirect('order_detail', order_id=order_id)
--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -4,7 +4,7 @@ from .views import product_list, product_detail
 app_name = 'shop'
 
 urlpatterns = [
-    path(" ", product_list, name="product_list"),
+    path("", product_list, name="product_list"),
     path("<slug:category_slug>/", product_list, name="product_list_by_category"),
     path("<int:id>/<slug:slug>/", product_detail, name="product_detail"),
 ]
