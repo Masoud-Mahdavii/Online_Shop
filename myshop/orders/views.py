877a424caea0cc9ea0f0e8efd44ecc6f5d7065d1
masoud mahdavi <masoudmahdavi1993@gmail.com>
2025-11-12 15:26:32 +0330

    Build the payment process

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -17,7 +17,10 @@ def order_create(request):
             cart.clear()
             # Launch asynchronous task
             order_created.delay(order.id)
-            return render(request, 'orders/order/created.html', {'order': order})
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect to the payment
+            return redirect('payment:process')
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
